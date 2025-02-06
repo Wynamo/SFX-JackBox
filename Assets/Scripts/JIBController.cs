80234c5345011967c189bcc501488be556fc9757
Will Conrad <144450261+Wynamo@users.noreply.github.com>
2025-02-06 14:09:56 -0400

    Particles Added

--- Assets/Scripts/JIBController.cs
+++ Assets/Scripts/JIBController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class JIBController : MonoBehaviour
 {
     public Animator animator;             // Animator for the jack-in-the-box animation
-    /*public ParticleSystem confetti;*/       // Particle system for the confetti
-    /*public float confettiDelay = 2.0f;*/    // Time delay before confetti plays (2 seconds after animation starts)
+    public ParticleSystem confetti;       // Particle system for the confetti
+    public float confettiDelay = 2.0f;    // Time delay before confetti plays (2 seconds after animation starts)
     private bool isPlaying = false;
     public AudioSource musicBox;
 
@@ -22,13 +22,13 @@ public class JIBController : MonoBehaviour
     }
 
     // Coroutine to delay confetti trigger
-    /*
+    
     IEnumerator PlayConfettiWithDelay(float delay)
     {
         yield return new WaitForSeconds(delay); // Wait for the specified delay
         confetti.Play();                        // Play the confetti particle system
     }
-    */
+    
 }
 
 
